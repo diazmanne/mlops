6d6d6450068d4d248d388e29f2340b072825e6d3
Manné Díaz <10931032+diazmanne@users.noreply.github.com>
2025-01-14 16:59:14 -0600

    setup_DAGSHub_experiemts

--- elastic-net.py
+++ elastic-net.py
@@ -1,4 +1,3 @@
-
 import os
 import warnings
 import sys
@@ -71,6 +70,10 @@ if __name__ == "__main__":
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
 
+        
+        # For remote server only (Dags)
+        remote_server_uri = "https://dagshub.com/diazmanne/mlops.mlflow"
+        mlflow.set_tracking_uri(remote_server_uri)
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
         # Model registry does not work with file store
